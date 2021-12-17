bd9b447075557e3e9347fbe295478253379b05de
dvdllyd <90646478+dvdllyd@users.noreply.github.com>
2021-12-16 19:57:04 -0600

    header and comp
    working navbar and some formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'profile', component:ProfileComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: '', redirectTo: "home", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
